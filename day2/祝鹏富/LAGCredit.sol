d677af4d4ca5658503bfcb19246fce5492b0aba6
Voilance <voilance@qq.com>
2019-06-24 12:34:04 +0800

    添加并修改祝鹏富的报告

--- day2/祝鹏富/LAGCredit.sol
+++ day2/祝鹏富/LAGCredit.sol
@@ -2,47 +2,36 @@ pragma solidity ^0.4.25;
 
 contract LAGCredit {
     
-    string name = "LAGC";
-    string symbol = "LAG";
-    uint256 totalSupply;
+    string name = "LAGCredit";
+    string symbol = "LAGC";
+    uint256 totalCredit;
+    mapping(address => uint256) private credits;
     
-    // address -> balances
-    mapping(address => uint256) private balances;
-    
-    // tell client the transaction happened.
-    event transferEvent(address from, address to, uint256 value);
-    
-    constructor(uint256 iniSupply, string iniName, string iniSymbol) public {
-        totalSupply = iniSupply;
-        balances[msg.sender] = totalSupply;
+    constructor(string iniName, string iniSymbol, uint256 iniCredit) public {
         name = iniName;
         symbol = iniSymbol;
+        totalCredit = iniCredit;
+        credits[msg.sender] = totalCredit;
     }
     
-    function getTotalSupply() constant returns (uint256) {
-        return totalSupply;
+    event transferEvent(address from, address to, uint256 value);
+    
+    function transfer(address toAddr, uint256 value) public {
+        _transfer(msg.sender, toAddr, value);
     }
     
-    function _transfer(address _from, address _to, uint _value) internal {
+    function _transfer(address _from, address _to, uint256 _value) internal {
         require(_to != 0x0);
-        require(balances[_from] >= _value);
-        require(balances[_to] + _value > balances[_to]);
+        require(credits[_from] >= _value);
+        require(credits[_to] + _value > credits[_to]);
         
-        uint preBalances = balances[_from] + balances[_to];
+        uint256 preCredit = credits[_from] + credits[_to];
         
-        balances[_from] -= _value;
-        balances[_to] += _value;
+        credits[_from] -= _value;
+        credits[_to] += _value;
         
         transferEvent(_from, _to, _value);
-        assert(balances[_from] + balances[_to] == preBalances);
-    }
-    
-    function transfer(address _to, uint256 _value) public {
-        _transfer(msg.sender, _to, _value);
-    }
-    
-    function balanceOf(address _owner) constant returns (uint256) {
-        return balances[_owner];
+        assert(credits[_from] + credits[_to] == preCredit);
     }
     
     function getName() constant returns (string) {
@@ -51,5 +40,17 @@ contract LAGCredit {
     
     function getSymbol() constant returns (string) {
         return symbol;
+    }
+    
+    function getTotalCredit() constant returns (uint256) {
+        return totalCredit;
+    }
+    
+    function getCredit(address addr) constant returns (uint256) {
+        return credits[addr];
+    }
+    
+    function getAddr() constant returns (address) {
+        return msg.sender;
     }
 }
---
+++ day2/祝鹏富/week4.md
@@ -0,0 +1,8 @@
+# <center>微众银行区块链实训</center>
+### <center>第四周周报</center>
+
+## 本周工作内容
+#### 本周工作内容
+1. 继续学习solidity语言以及智能合约编写。
+1. 在终端利用FISCO-BCOS控制台部署编写的智能合约。
+2. 利用spring-boot-starter尝试配置sdk。
